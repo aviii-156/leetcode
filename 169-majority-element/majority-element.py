fe569b431b7aeebd8005d4741428839caba74271
Abhishek Kumar Singh <abhishekkumar12ka4@gmail.com>
2026-09-11 22:37:45 +0530

    Time: 5 ms (65.86%) | Memory: 21.3 MB (18.85%) - LeetSync

---
+++ 169-majority-element/majority-element.py
@@ -0,0 +1,10 @@
+class Solution:
+    def majorityElement(self, nums):
+        freq = {}
+
+        for num in nums:
+            freq[num] = freq.get(num, 0) + 1
+
+        for num in freq:
+            if freq[num] > len(nums) // 2:
+                return num
